a72ea152527c4f6a3a7f2bac03219b2f3d0cbfdf
AkamexBii <trungtalon31@gmail.com>
2024-02-21 02:23:08 +0700

    day la nhanh cua trung

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private float Move;
+
+    public float jump;
+
+    public bool isJumping; 
+    private Rigidbody2D rb;
+
+    private Vector2 respawnPoint;
+    public GameObject fallDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        respawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            Debug.Log("jump");
+
+            fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;    
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("FallDetector")) 
+        {
+            transform.position = respawnPoint;
+        }
+    }
+
+}
